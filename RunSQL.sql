455b2545966057ead3c9c49977722cb9990d713b
Wolf Moritz <wolfmoritz@gmail.com>
2013-07-03 14:01:40 -0700

    Update RunSQL.sql
    Added `set feedback off` to suppress the `no rows selected` message when there are no errors.

--- RunSQL.sql
+++ RunSQL.sql
@@ -1,6 +1,7 @@
 SET LINESIZE 2000
 SET PAGESIZE 0
 SET VERIFY OFF
+SET FEEDBACK OFF
 @&1
 SELECT 'Filename: &1' FROM DUAL;
 SELECT '(' || TYPE || ' ' || NAME || '/0:' || POSITION || ')' || ' ' || LINE || ':' || POSITION || ' ' || TEXT as ERRORS
