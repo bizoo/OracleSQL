d6383a1f771b06ec31463a882ffa46cbb6271531
Mike Leonard <mike.leonard@fivium.co.uk>
2014-07-03 17:22:52 +1000

    Reverted accidental remove of changes made to RunSQL.sql.

--- RunSQL.sql
+++ RunSQL.sql
@@ -4,7 +4,7 @@ SET VERIFY OFF
 SET FEEDBACK OFF
 @&1
 SELECT 'Filename: &1' FROM DUAL;
-SELECT '(' || TYPE || ' ' || NAME || '/' || LINE || ':' || POSITION || ')' || ' ' || LINE || ':' || POSITION || ' ' || TEXT as ERRORS
+SELECT '(' || TYPE || ' ' || NAME || '/0:' || POSITION || ')' || ' ' || LINE || ':' || POSITION || ' ' || TEXT as ERRORS
   FROM ALL_ERRORS
  WHERE line <> 0
    AND (
